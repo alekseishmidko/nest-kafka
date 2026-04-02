511955f3f52397c49ec958e028f75e76eb3be08a
alekseishmidko <alexeishmidko@gmail.com>
2026-04-03 00:26:46 +0700

    refactor: split backend into apps and libs

---
+++ backend/libs/common/src/index.ts
@@ -0,0 +1,9 @@
+export * from './common.module';
+export * from './common.service';
+
+//
+// export * from './dto';
+
+export * from './constants';
+export * from './interfaces';
+export * from './utils';
